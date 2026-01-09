27cbaf6135ee019f5a6b87be36f9e7ddfed5d8e3
bakane <dyatlovleonid@gmail.com>
2026-01-09 16:30:17 +0300

    xd

--- src/drawing.rs
+++ src/drawing.rs
@@ -1,38 +1,64 @@
-use sdl3::Error;
-use sdl3::pixels::{Color};
+use glam::{vec2, Vec2};
+use sdl3::pixels::Color;
 use sdl3::render::{Canvas, FPoint, RenderTarget, Vertex};
-use crate::vec::{vec2, Vec2};
+use sdl3::Error;
 
 pub trait CanvasExt {
-    fn draw_thick_lines<'a, P: Into<&'a [Vec2]>>(&mut self, points: P, thickness: f32, color: Color) -> Result<(), sdl3::Error>;
+    fn draw_thick_lines(
+        &mut self,
+        points: &[Vec2],
+        thickness: f32,
+        color: Color,
+    ) -> Result<(), Error>;
 }
 
-impl <T: RenderTarget> CanvasExt for Canvas<T> {
-    fn draw_thick_lines<'a, P: Into<&'a [Vec2]>>(&mut self, points: P, thickness: f32, color: Color) -> Result<(), Error> {
-        let p = points.into();
+impl<T: RenderTarget> CanvasExt for Canvas<T> {
+    fn draw_thick_lines(
+        &mut self,
+        points: &[Vec2],
+        thickness: f32,
+        color: Color,
+    ) -> Result<(), Error> {
         let half_thickness = thickness / 2.0;
         let fcolor = color.into();
 
         self.set_draw_color(color);
 
-        let mut iter = p.windows(2);
+        let mut deltas = Vec::with_capacity(points.len() - 1);
+
+        let mut iter = points.windows(2);
         while let Some([p1, p2]) = iter.next() {
             let angle = f32::atan2(p2.y - p1.y, p2.x - p1.x);
-            let dx = half_thickness * angle.sin();
-            let dy = -half_thickness * angle.cos();
+            let d = vec2(half_thickness * angle.sin(), -half_thickness * angle.cos());
 
-            let vertices = [
-                vec2(p1.x - dx, p1.y - dy),
-                vec2(p1.x + dx, p1.y + dy),
-                vec2(p2.x - dx, p2.y - dy),
-                vec2(p2.x + dx, p2.y + dy),
-            ].map(|v| Vertex {
-                position: v.into(),
+            deltas.push(d);
+            let vertices = [p1 - d, p1 + d, p2 - d, p2 + d].map(|v| Vertex {
+                position: FPoint::new(v.x, v.y),
                 color: fcolor,
-                tex_coord: FPoint::new(0.0, 0.0)
+                tex_coord: FPoint::new(0.0, 0.0),
             });
 
-            self.render_geometry(&vertices, None, &[0, 1, 2, 1, 2, 3])?
+            self.render_geometry(&vertices, None, &[0u8, 1, 2, 1, 2, 3])?;
+        }
+
+        let mut iter = points.windows(3).enumerate();
+        while let Some((i, [p1, p2, p3])) = iter.next() {
+            let v1 = p1 - p2;
+            let v2 = p3 - p2;
+
+            let sign = v1.perp_dot(v2).signum();
+            if sign == 0.0 {
+                continue;
+            }
+
+            let vertices =
+                [*p2, p2 + deltas[i] * -sign, p2 + deltas[i + 1] * -sign].map(|v| Vertex {
+                    position: FPoint::new(v.x, v.y),
+                    color: fcolor,
+                    tex_coord: FPoint::new(0.0, 0.0),
+                });
+
+            self.render_geometry(&vertices, None, &[0u8, 1, 2])?;
         }
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 mod drawing;
-mod vec;
 
-use sdl3::pixels::Color;
+use crate::drawing::CanvasExt;
+use glam::{vec2, Vec2};
 use sdl3::event::Event;
 use sdl3::keyboard::Keycode;
+use sdl3::pixels::Color;
+use sdl3::EventPump;
 use std::time::Duration;
-use sdl3::render::{FPoint, FRect};
-use crate::drawing::CanvasExt;
-use crate::vec::vec2;
 
 pub fn main() {
     let sdl_context = sdl3::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl3 demo", 800, 600)
+    let window = video_subsystem
+        .window("rust-sdl3 demo", 800, 600)
         .position_centered()
         .build()
         .unwrap();
@@ -25,24 +25,42 @@ pub fn main() {
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
-    'running: loop {
+
+    let mut mouse_positions = vec![mouse_pos(&event_pump)];
+
+    loop {
         i = (i + 1) % 255;
         canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    return;
+                }
                 _ => {}
             }
         }
         // The rest of the game loop goes here...
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.draw_thick_lines([vec2(100.0, 100.0), vec2(200.0, 100.0), vec2(300.0, 200.0)].as_slice(), 50.0, Color::RGB(255, 255, 255)).unwrap();
+        mouse_positions.push(mouse_pos(&event_pump));
+        if mouse_positions.len() > 100 {
+            mouse_positions = mouse_positions[mouse_positions.len() - 100..].to_vec();
+        }
+
+        canvas
+            .draw_thick_lines(&mouse_positions, 5.0, Color::RGB(255, 255, 255))
+            .unwrap();
 
         canvas.present();
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 144));
     }
+}
+
+fn mouse_pos(event_pump: &EventPump) -> Vec2 {
+    let mouse_state = event_pump.mouse_state();
+    vec2(mouse_state.x(), mouse_state.y())
 }
